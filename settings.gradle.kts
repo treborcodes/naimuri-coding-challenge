4e1df5b34d6693cea753df4fcdf7849fc495919e
Rob Hughes <rdhughes93@gmail.com>
2026-05-22 12:18:17 +0100

    Squash commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,5 +10,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-rootProject.name = "naimuri-coding-challenge"
+rootProject.name = "wordsquare"
 include("app")
